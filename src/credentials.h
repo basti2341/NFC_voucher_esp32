dd50dc5c9b4b57974a530e8467ef4f4a31ebb8de
basti2341 <bastirosi62@gmail.com>
2026-03-12 14:26:42 +0100

    Update credentials.h

--- src/credentials.h
+++ src/credentials.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define WIFI_SSID "Student" ///< SSID des WLANs ... bitte anpassen
-#define WIFI_USER "musty" ///< Benutzername für WPA2-Enterprise ... bitte anpassen
-#define WIFI_PASSWORD "bubu" ///< Passwort für WPA2-Enterprise ... bitte anpassen
+#define WIFI_USER "S.Rosenbichler" ///< Benutzername für WPA2-Enterprise ... bitte anpassen
+#define WIFI_PASSWORD "Rosi2008!" ///< Passwort für WPA2-Enterprise ... bitte anpassen
 
 
 const char *AP_SSID = "NFC-ESP32";///< SSID des Accesspoint ... bitte anpassen
