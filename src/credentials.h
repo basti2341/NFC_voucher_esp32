3ba86e0b990253caf793c7b70d433332a6e5d2fc
basti2341 <bastirosi62@gmail.com>
2026-03-15 11:38:57 +0100

    Update credentials.h

--- src/credentials.h
+++ src/credentials.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define WIFI_SSID "Galaxy S25 Ultra von Basti" ///< SSID des WLANs ... bitte anpassen
-#define WIFI_USER "" ///< Benutzername für WPA2-Enterprise ... bitte anpassen
-#define WIFI_PASSWORD "basti2008!" ///< Passwort für WPA2-Enterprise ... bitte anpassen
+#define WIFI_SSID "Student" ///< SSID des WLANs ... bitte anpassen
+#define WIFI_USER "dummy" ///< Benutzername für WPA2-Enterprise ... bitte anpassen
+#define WIFI_PASSWORD "dummy" ///< Passwort für WPA2-Enterprise ... bitte anpassen
 
 
 const char *AP_SSID = "NFC-ESP32";///< SSID des Accesspoint ... bitte anpassen
